814b576359c5dc4d7b23562bd0e198e4f56d53db
CodeWanderer <57658021+Ivan602@users.noreply.github.com>
2020-06-26 10:12:16 +0900

    Update marioNET_client.lua

--- marioNET_client.lua
+++ marioNET_client.lua
@@ -1,6 +1,6 @@
 -- MarioNET v1.0 by CodeWanderer (2020)
 -- See explanation in "marioNET.lua"
---	*** Hosted on my GitHub repository https://github.com/Ivan602/Code-Wanderer-s-Repo ***
+--	*** Hosted on my GitHub repository https://github.com/Ivan602/marioNET ***
 
 -- #############################################################################
 -- SETTINGS
